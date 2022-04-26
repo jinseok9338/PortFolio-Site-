a15f3f34129d8ee674b8599c91109be248a5af50
jinseok9338 <jinseok9338@gmail.com>
2022-04-26 13:31:49 +0000

    rust practice

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,16 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
-mod todo_functions;
-mod server_functions;
 
-
-
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .service(server_functions::createTodos)
-            .route("/", web::get().to(server_functions::manual_hello))
-    })
-    .bind("127.0.0.1:8080")?
-    .run()
-    .await
+fn main() {
+    let s = String::from("hello");
+    let number = 5;
+    take_ownerShip(s);
+    println!("{}",s)
+    make_Copy(number)
 }
 
+fn take_ownerShip(some_string:String){
+    println!("{}",some_string)
+}
 
-// let mut todosConstruct = todo_functions::Todos {
-//     todos: Vec::new()
-// };
-
-
-// todosConstruct.add_todo("My first Todo");
-// todosConstruct.add_todo("My second Todo");
-
-// todosConstruct.delete_todo(2);
-// todosConstruct.toggle_done(1);
-// todosConstruct.show_todos();
-// todosConstruct.toggle_done(1); // This is problem
-// todosConstruct.show_todos();
+fn make_Copy(some_integer:i32){
+    println!("{}",some_integer)
+}
