664be074e2e3842173c557dedcf921a12048b49c
jinseok9338 <jinseok9338@gmail.com>
2022-05-09 13:45:04 +0000

    Do not use canvas for yew for the time being

--- yew-app/src/main.rs
+++ yew-app/src/main.rs
@@ -1,42 +1,40 @@
+use gloo_utils::document;
 use yew::prelude::*;
 
-enum Msg {
-    AddOne,
+
+struct Display {
+  
 }
 
-struct Model {
-    value: i64,
-}
-
-impl Component for Model {
-    type Message = Msg;
+impl Component for Display {
+    type Message = ();
     type Properties = ();
 
     fn create(_ctx: &Context<Self>) -> Self {
-        Self {
-            value: 0,
-        }
+        Self {}
     }
 
-    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
-        match msg {
-            Msg::AddOne => {
-                self.value += 1;
-                true
-            }
-        }
+    fn update(&mut self, _ctx: &Context<Self>, _msg: Self::Message) -> bool {
+        
+        false
     }
 
-    fn view(&self, ctx: &Context<Self>) -> Html {
+    fn view(&self, _ctx: &Context<Self>) -> Html {
         html! {
-            <div>
-                <button onclick={ctx.link().callback(|_| Msg::AddOne)}>{ "+1" }</button>
-                <p>{ self.value }</p>
-            </div>
+          
+           <canvas id ="card">
+
+           </canvas>
+           
         }
+    }
+
+    fn rendered(&mut self, ctx: &Context<Self>, first_render: bool) {
+        let canvas = document().query_selector("#card").unwrap().unwrap();
+        let canvas_context: () = canvas.set_inner_html("hello");
     }
 }
 
 fn main() {
-    yew::start_app::<Model>();
+    yew::start_app::<Display>();
 }
