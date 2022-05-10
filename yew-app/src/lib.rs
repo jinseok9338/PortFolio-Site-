0da339ebfcb9fdb3d375cfd62621a8a04acfea6f
jinseok9338 <jinseok9338@gmail.com>
2022-05-10 14:09:16 +0000

    console.log in yew

---
+++ yew-app/src/lib.rs
@@ -0,0 +1,27 @@
+use yew::prelude::*;
+
+
+pub struct App {
+ name: String,
+}
+
+impl Component for App  {
+    type Message =();
+
+    type Properties =();
+
+    fn create(_ctx: &Context<Self>) -> Self {
+        let name = "Hello world".to_owned();
+        gloo::console::console!(name.clone()); 
+        Self {name}
+    }
+
+    
+    fn view(&self ,_ctx: &Context<Self>) -> Html {
+        html! {
+            <div class ={classes!("flex", "align-center", "justify-center")} >
+                <h1 class={classes!("bg-red-100")} >{&self.name}</h1>
+            </div>
+         }
+    }
+}
--- yew-app/src/main.rs
+++ yew-app/src/main.rs
@@ -1,40 +1,9 @@
 use gloo_utils::document;
 use yew::prelude::*;
+use yew_app::App;
 
 
-struct Display {
-  
-}
-
-impl Component for Display {
-    type Message = ();
-    type Properties = ();
-
-    fn create(_ctx: &Context<Self>) -> Self {
-        Self {}
-    }
-
-    fn update(&mut self, _ctx: &Context<Self>, _msg: Self::Message) -> bool {
-        
-        false
-    }
-
-    fn view(&self, _ctx: &Context<Self>) -> Html {
-        html! {
-          
-           <canvas id ="card">
-
-           </canvas>
-           
-        }
-    }
-
-    fn rendered(&mut self, ctx: &Context<Self>, first_render: bool) {
-        let canvas = document().query_selector("#card").unwrap().unwrap();
-        let canvas_context: () = canvas.set_inner_html("hello");
-    }
-}
 
 fn main() {
-    yew::start_app::<Display>();
+    yew::start_app::<App>();
 }
