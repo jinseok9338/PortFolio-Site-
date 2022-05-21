876438455fee5b726b01b5e397a77863263a0ab7
jinseok9338 <jinseok9338@gmail.com>
2022-05-21 11:54:42 +0000

    Making navbar today

--- yew-app/src/components/header.rs
+++ yew-app/src/components/header.rs
@@ -5,7 +5,22 @@ use yew::prelude::*;
 #[function_component(HeaderContents)]
 pub fn header_contents() ->Html {
  html!{
-        <div class={classes!("flex","w-full", "h-full", "bg-[#ff00e1]")}>{"This is header"}</div>
+        <div class={classes!("flex", "h-full")}>
+            <div class={classes!("left-container","h-full","w-16","bg-[#eb4034]","flex","items-center","justify-center")}>
+                <span>{"J.J.J"}</span>
+            </div>
+            <div class={classes!("right-container", "w-auto","h-full","bg-[#32a852]","items-center","flex","justify-center")}>
+                <div>
+                    <ul>
+                        <li class={classes!("inline-block")}><a href="#" >{"Home"} <span class="sr-only">{"(current)"}</span></a></li>
+                        <li class={classes!("inline-block","ml-[1rem]")}><a href="#">{"Home"}</a></li>
+                        <li class={classes!("inline-block","ml-[1rem]")}><a href="#">{"Home"}</a></li>
+                        <li class={classes!("inline-block","ml-[1rem]")}><a href="#">{"Home"}</a></li>
+                        <li class={classes!("inline-block","ml-[1rem]")}><a href="#">{"Home"}</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
  }
 }
 
@@ -28,9 +43,9 @@ impl Component for Header {
     fn view(&self, _ctx: &Context<Self>) -> Html {
        
         html! {
-            <div class={classes!("header-container","w-screen","bg-[black]","h-[30px]")}>
+            <nav class={classes!("header-container","w-screen","md:block","hidden","h-[100px]")}>
             <HeaderContents/>
-         </div>
+         </nav >
         }
     }
 }
