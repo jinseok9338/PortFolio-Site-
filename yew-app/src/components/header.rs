faa7755a861e283e89e054a4aa52f9c6bfbeee49
jinseok9338 <jinseok9338@gmail.com>
2022-05-21 13:57:45 +0000

    Making header

--- yew-app/src/components/header.rs
+++ yew-app/src/components/header.rs
@@ -4,19 +4,36 @@ use yew::prelude::*;
 
 #[function_component(HeaderContents)]
 pub fn header_contents() ->Html {
+    let color_theme = use_state(|| "LightMode".to_owned());
+    let toggle_theme = Callback::from(move |_:&str| 
+        {   
+            let cloned_color_theme = color_theme.clone();
+            let new_theme = if *cloned_color_theme == "LightMode" { "DarkMode" } else { "LightMode" };
+            cloned_color_theme.set(new_theme.to_owned())
+        
+        });
+        
  html!{
-        <div class={classes!("flex", "h-full")}>
+        <div class={classes!("flex", "h-full","w-full","pt-4","pb-4","pl-16","pr-16")}>
             <div class={classes!("left-container","h-full","w-16","bg-[#eb4034]","flex","items-center","justify-center")}>
-                <span>{"J.J.J"}</span>
+            
+                <span class={classes!("font-bold","text-lg")}>{"J.J.J"}</span>
             </div>
-            <div class={classes!("right-container", "w-auto","h-full","bg-[#32a852]","items-center","flex","justify-center")}>
+            <div class={classes!("right-container", "h-full","bg-[#32a852]","items-center","flex","justify-around","grow")}>
                 <div>
                     <ul>
-                        <li class={classes!("inline-block")}><a href="#" >{"Home"} <span class="sr-only">{"(current)"}</span></a></li>
-                        <li class={classes!("inline-block","ml-[1rem]")}><a href="#">{"Home"}</a></li>
-                        <li class={classes!("inline-block","ml-[1rem]")}><a href="#">{"Home"}</a></li>
-                        <li class={classes!("inline-block","ml-[1rem]")}><a href="#">{"Home"}</a></li>
-                        <li class={classes!("inline-block","ml-[1rem]")}><a href="#">{"Home"}</a></li>
+                        <li class={classes!("inline-block")}><a href="#" >{"About"}</a></li>
+                        <li class={classes!("inline-block","ml-[1rem]")}><a href="#">{"Projects"}</a></li>
+                        <li class={classes!("inline-block","ml-[1rem]")}><a href="#">{"Resume"}</a></li>
+                        <li class={classes!("inline-block","ml-[1rem]")}><a href="#">{"Contact"}</a></li>
+                        <li class={classes!("inline-block","ml-auto")}>
+                        if *color_theme == "Light Theme" {
+                            <i class="uil uil-sunset"></i>
+                        }else{
+                            <i class="uil uil-moon"></i>
+                        }
+                        <button {toggle_theme}>{&*color_theme}</button>
+                        </li>
                     </ul>
                 </div>
             </div>
